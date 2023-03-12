b65835ef9eb6031900f0228ea11486672f8d7cf1
Saptak Banerjee <125642304+SaptakBN@users.noreply.github.com>
2023-03-12 23:24:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+let x = Math.floor((Math.random() * 100) + 1);
+let chances = 0;
+
+function printInput() {
+    let number = document.getElementById("number").value;
+    number = Number.parseInt(number);
+
+    if (number < x) {
+        chances++;
+        document.getElementById("output").innerHTML = "The Number is higher";
+    }
+    else if (number > x) {
+        chances++;
+        document.getElementById("output").innerHTML = "The number is lower";
+    }
+    else {
+        chances++;
+        document.getElementById("output").innerHTML = "";
+        document.getElementById("times").innerHTML = "You guessed the number in " + chances + " attempts.";
+        $(document).ready(function () {
+            $("#myModal").modal("show");
+        });
+    }
+}
+
+var input = document.getElementById("number");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
+
